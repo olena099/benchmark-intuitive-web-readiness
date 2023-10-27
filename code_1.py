9487266903c8dcd23d832828f48767136711f4dc
olena099 <142909583+olena099@users.noreply.github.com>
2023-10-26 21:22:03 -0700

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,25 @@
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        }
+
+    public static void main(String[] args) {
+}
+        int[] data = new int[10];
+        }
+    }
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        for (int item : data) {        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+        int[] data = new int[10];
+        }
+}
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+public class RandomDataGenerator {
+
+        }
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
